1b7bc8dda242b3a9737de8b55f3156d4ad826aff
william rodrigues <williamr723@gmail.com>
2023-10-23 20:48:02 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { TitleComponent } from './components/title/title.component';
+import { LogosComponent } from './components/logos/logos.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    TitleComponent,
+    LogosComponent,
+    GalleryComponent,
+
   ],
   imports: [
     BrowserModule,
