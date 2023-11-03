32720d7d20cfafb5b58646fb6cd97ad886158402
william rodrigues <williamr723@gmail.com>
2023-11-03 20:04:14 -0300

    link do projeto atualizado

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -14,7 +14,7 @@ export class GalleryComponent implements OnInit{
   projeto2:string = 'https://media.cnn.com/api/v1/images/stellar/prod/230518162326-01-us-parking-spots-restricted.jpg?c=original'
   title2:string = 'Vaga de estacionamento'
   description2:string = 'Parking spot ou Vaga de estacionamento foi feito destinado a cadastrar pessoa seu carro seu expectiva vaga. Tecnologia usada foi springboot usando springsecurity para apenas o admin possa acessa documentação e fazer post update e delete e o get foi feito com paginação e o banco de dados usado e o postgresql.'
-  link2:string = 'https://park-rvud.onrender.com/swagger-ui/index.html#/'
+  link2:string = 'https://parking-spot-umber.vercel.app/'
   constructor() {
 
   }
