0264c5f502bb1663c8faf014c093a943addbcdc9
william rodrigues <williamr723@gmail.com>
2023-11-26 16:10:15 -0300

    atualizando tempo de dev

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -9,7 +9,7 @@ export class CardComponent implements OnInit{
 
   title:string = 'Ola, me chamo'
   description:string = 'William Rodrigues'
-  paragraph:string = 'Sou dev de java com spring boot desenvolvedor a 1 ano 4 meses.'
+  paragraph:string = 'Sou dev de java com spring boot desenvolvedor a 2 anos.'
   Contact:string = 'Contato'
   photo:string = 'https://scontent.fqnv5-1.fna.fbcdn.net/v/t39.30808-6/217553092_828481401132251_8388150708981078787_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Uo32FHFt67IAX8XFnWv&_nc_ht=scontent.fqnv5-1.fna&oh=00_AfCUCOAk6P-rKE9vRL4VAt0onGKSq_RTxdTc89GmtjZ-xQ&oe=65695E94'
   constructor() {
