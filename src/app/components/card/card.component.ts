02d7708ecd4026eb5f4e187b6b0611a5da3069e9
william rodrigues <williamr723@gmail.com>
2024-01-08 00:40:06 -0300

    add linguagem

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -9,7 +9,7 @@ export class CardComponent implements OnInit{
 
   title:string = 'Ola, me chamo'
   description:string = 'William Rodrigues'
-  paragraph:string = 'Sou dev de java com spring boot , angular , python e django desenvolvedor a 2 anos.'
+  paragraph:string = 'Sou dev de java spring boot , angular , python django e php laravel desenvolvedor a 2 anos.'
   Contact:string = 'Contato'
   photo:string = 'https://scontent.fqnv5-1.fna.fbcdn.net/v/t1.6435-9/123372752_689546285025764_7419566225712081602_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=7a1959&_nc_eui2=AeGCPUetzIgr-VJLNzmUnoOcsVXMKhnLnxKxVcwqGcufElI_zYbHFagdvZEc0mRBPIn8F3m6IGpOT9KOU0WgEbHJ&_nc_ohc=8xEDloLPSVIAX-kTnfT&_nc_ht=scontent.fqnv5-1.fna&oh=00_AfBgejD6VbHWM6b-7ie3H3gGhNLh32vtSZHa0RZ3GYdkAw&oe=659DEBA2'
   constructor() {
