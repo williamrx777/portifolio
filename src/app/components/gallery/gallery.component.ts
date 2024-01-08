97c5f3cf4ab08fcdfa16747516b8c89e3e49de1a
william rodrigues <williamr723@gmail.com>
2024-01-08 00:42:55 -0300

    atualizando descrição

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -9,11 +9,11 @@ export class GalleryComponent implements OnInit{
   top:string = 'Latest work'
   projeto1:string ='https://img.freepik.com/vetores-premium/logotipo-vintage-para-comida-e-bebida-do-restaurante_116238-69.jpg'
   title:string = 'Caferia'
-  description:string = 'Cafeteria foi feito para uma pessoa em ambiente presencial adiciona suas vendas. Tecnologias usadas foi angular para frontend e o springboot para o backend.'
+  description:string = 'Cafeteria tecnologias usadas foi angular para frontend e o laravel para o backend.'
   link:string = 'https://cafeteria-alpha.vercel.app/'
   projeto3:string = 'https://t2.tudocdn.net/687837?w=824&h=494'
   title3:string = 'Filmes e animes'
-  description3:string = 'Filmes e site em desenvolvimento de filmes e animes.'
+  description3:string = 'Filmes tecnologias usadas foi angular para frontend e o django para o backend.'
   link3:string = 'https://filmes-delta.vercel.app/'
   constructor() {
 
