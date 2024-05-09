7fa91a1dcf7381607d0ba6c32fc7172b426b9e7e
william rodrigues <williamr723@gmail.com>
2024-05-09 10:17:11 -0300

    refatorando codigo

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -9,9 +9,9 @@ export class CardComponent implements OnInit{
 
   title:string = 'Ola, me chamo'
   description:string = 'William Rodrigues'
-  paragraph:string = 'Sou dev de java spring boot , angular , python django e php laravel desenvolvedor a 2 anos.'
+  paragraph:string = 'Sou dev de java spring boot , angular , python django e nodejs desenvolvedor a 2 anos.'
   Contact:string = 'Contato'
-  photo:string = 'https://scontent.fqnv5-1.fna.fbcdn.net/v/t1.6435-9/123372752_689546285025764_7419566225712081602_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=7a1959&_nc_eui2=AeGCPUetzIgr-VJLNzmUnoOcsVXMKhnLnxKxVcwqGcufElI_zYbHFagdvZEc0mRBPIn8F3m6IGpOT9KOU0WgEbHJ&_nc_ohc=8xEDloLPSVIAX-kTnfT&_nc_ht=scontent.fqnv5-1.fna&oh=00_AfBgejD6VbHWM6b-7ie3H3gGhNLh32vtSZHa0RZ3GYdkAw&oe=659DEBA2'
+  photo:string = 'https://media.licdn.com/dms/image/C4E03AQG_RCnOrGYRpA/profile-displayphoto-shrink_800_800/0/1607630829179?e=1720656000&v=beta&t=IXSWdpis2cj0BzLaqjoGL2NN85zh4uf9mbePYVL6LHk'
   constructor() {
 
   }
