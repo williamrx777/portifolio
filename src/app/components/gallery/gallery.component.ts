a2e05d71eb02cda9de9a7ba1766f97fc587d771b
william rodrigues <38387964+williamrx777@users.noreply.github.com>
2025-05-18 12:31:10 -0300

    trocando icon

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -31,7 +31,7 @@ export class GalleryComponent implements OnInit{
   title6:string = 'Adote um Pet'
   description6:string = 'Sistema de adoção de pets tecnologias usadas foi django e postgres.'
   link6:string = 'https://projects-jhb3.onrender.com/adote/usuario/logar/'
-  projeto7:string = '🛍️'
+  projeto7:string = 'https://images.emojiterra.com/microsoft/fluent-emoji/15.1/1024px/1f6cd_color.png'
   title7:string = 'Nossa Vitrine'
   description7:string = 'Site de ecommerce tecnologias usadas foi django e postgres.'
   link7:string = 'https://ecommerce-n4s3mtqsv-williamrx777s-projects.vercel.app/'
